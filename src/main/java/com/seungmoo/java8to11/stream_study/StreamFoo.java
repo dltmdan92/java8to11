d6699c4944b3c59a57a2b5aea0907b332227b304
seungmoo.lee <seungmoo.lee@lotte.net>
2020-12-31 14:40:06 +0900

    Stream API 2

--- src/main/java/com/seungmoo/java8to11/stream_study/StreamFoo.java
+++ src/main/java/com/seungmoo/java8to11/stream_study/StreamFoo.java
@@ -1,8 +1,11 @@
 package com.seungmoo.java8to11.stream_study;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamFoo {
 
@@ -46,6 +49,9 @@ public class StreamFoo {
         collect.forEach(System.out::println);
     }
 
+    /**
+     * stream API 를 좀 더 사용해보자
+     */
     public void streamAPI2() {
         List<OnlineClass> springClasses = new ArrayList<>();
         springClasses.add(new OnlineClass(1, "spring boot", true));
@@ -56,12 +62,25 @@ public class StreamFoo {
 
         System.out.println("spring으로 시작하는 수업");
         // TODO
+        springClasses.stream()
+                // filter operation
+                .filter(onlineClass -> onlineClass.getTitle().startsWith("spring"))
+                .forEach(onlineClass -> System.out.println(onlineClass.getId()));
 
         System.out.println("close 되지 않은 수업");
         // TODO
+        springClasses.stream()
+                //.filter(onlineClass -> !onlineClass.isClosed())
+                // 이렇게 사용할 수 있다. (아쉽게도 메서드 레퍼런스에는 not이 없으므로 Predicate를 활용하자.)
+                .filter(Predicate.not(OnlineClass::isClosed))
+                .forEach(onlineClass -> System.out.println(onlineClass.getId()));
 
         System.out.println("수업 이름만 모아서 스트림 만들기");
         // TODO
+        springClasses.stream()
+                // map operator에서 string 타입으로 변경하여 return 한다. --> 그 다음 오퍼레이터는 map이 변경한 타입으로 받는다.
+                .map(OnlineClass::getTitle)
+                .forEach(System.out::println);
 
         List<OnlineClass> javaClasses = new ArrayList<>();
         javaClasses.add(new OnlineClass(6, "The Java Test", true));
@@ -72,27 +91,40 @@ public class StreamFoo {
         keesunEvents.add(springClasses);
         keesunEvents.add(javaClasses);
 
-        System.out.println("spring으로 시작하는 수업");
-        // TODO
-
-        System.out.println("close 되지 않은 수업");
-        // TODO
-
-        System.out.println("수업 이름만 모아서 스트림 만들기");
-        // TODO
-
+        /**
+         * List<List>> 타입의 Collection을 stream으로 만들면
+         * 스트림 내부에는 여러개의 List들이 원소로 존재하게 된다.  [ List, List, List ]
+         * 여러개의 List들을 Flatten 시키는 작업 --> FlatMap 이다. 하나의 스트림으로 flatten 시키는 것임.
+         * [ List, List, List ] --> [ onlineClass, onlineClass, onlineClass, onlineClass, .... ]
+         */
 
         System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
         // TODO
+        keesunEvents.stream()
+                .flatMap(Collection::stream)
+                .forEach(onlineClass -> System.out.println(onlineClass.getId()));
 
         System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
         // TODO
+        Stream.iterate(10, i -> i + 1) // 이렇게 까지만 하면 무제한 스트림이 생성된다. (물론 중개형 opertor 이므로 실행은 안함.)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
 
         System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
         // TODO
+        boolean test = javaClasses.stream()
+                .anyMatch(onlineClass -> onlineClass.getTitle().contains("Test"));
+        System.out.println(test);
 
-        System.out.println("스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기");
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
         // TODO
+        List<String> spring = springClasses.stream()
+                .map(OnlineClass::getTitle)
+                .filter(title -> title.contains("spring"))
+                .collect(Collectors.toList());
+        
+        spring.forEach(System.out::println);
     }
 
 }
